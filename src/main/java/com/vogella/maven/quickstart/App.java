438b1834a2a4eb8bcb40311e6b6f5db4babd61d3
Sabrina <sabrinazuraimi@outlook.com>
2016-12-19 16:04:06 +0900

    commit

--- src/main/java/com/vogella/maven/quickstart/App.java
+++ src/main/java/com/vogella/maven/quickstart/App.java
@@ -5,13 +5,81 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.JFrame;
+
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.tasks.OnSuccessListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class frame extends JFrame implements ActionListener{
+	JPanel panel = new JPanel();
+	JTextField username = new JTextField(20);
+	JTextField password = new JTextField(20);
+	
+	JLabel email = new JLabel("");
+	JLabel pass = new JLabel("");
+	
+	JButton login = new JButton("Log In");
+    JButton signup = new JButton("Sign Up");
+    
+
+	frame(){
+		setTitle("Log In Page");
+		login.addActionListener(this);
+		signup.addActionListener(this);
+        
+		panel.setLayout(new FlowLayout());
+		panel.add(email);
+		panel.add(username);
+		panel.add(pass);
+		panel.add(password);
+		panel.add(login);
+		panel.add(signup);
+		
+		
+		email.setText("E-mail:");
+		pass.setText("Password:");
+		getContentPane().add(panel);
+		
+		setSize(300,200);
+		setVisible(true);
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == login){
+			System.out.println("Tring to Log In");
+		    //Firebaseに元々入れたトークンを入れてみました
+			String sampletoken = "aHN9WZxWHrOJABL8Nues9SpoBfq2";
+			FirebaseAuth.getInstance().verifyIdToken(sampletoken).addOnSuccessListener(new OnSuccessListener<FirebaseToken>(){
+
+				public void onSuccess(FirebaseToken decodedToken) {
+					String uid = decodedToken.getUid();
+					System.out.println("Logged In!");
+					
+				}
+				
+			});
+			
+			}
+		else {
+		}
+		}
+		
+	}
+
 
 class User{
 	
@@ -20,16 +88,17 @@ class User{
 	public String nickname;
 	
 	public User(String date_of_birth, String full_name){
-		//..
+		this.date_of_birth = date_of_birth;
+		this.full_name = full_name;
 	}
 	
 	public User(String date_of_birth, String full_name, String nickname){
-		//...
+		this.date_of_birth = date_of_birth;
+		this.full_name = full_name;
+		this.nickname = nickname;
+		
 	}
 }
-
-
-
 
 
 public class App 
@@ -38,13 +107,15 @@ public class App
 	       /*Firebase SDKをinitializeするために*/
 	      FirebaseOptions options = new FirebaseOptions.Builder()
 	    		  .setServiceAccount(new FileInputStream("serviceAccounKey.json"))
-	    		  .setDatabaseUrl("https://javajikken.firebaseio.com/")
+	    		  .setDatabaseUrl("https://javajikken.firebaseio.com")
 	    		  .build();
 	      
 	      FirebaseApp.initializeApp(options);
 	  
+	      System.out.println("App Initialized!");
+	      
 	      //As an admin, the app has access to read and write all data,regardless of Security Rules//
-	      DatabaseReference ref = FirebaseDatabase.getInstance().getReference("restricted_acceess/secret_document");
+	      DatabaseReference ref = FirebaseDatabase.getInstance().getReference("user");
 	      ref.addListenerForSingleValueEvent(new ValueEventListener(){
 	    	  public void onDataChange(DataSnapshot dataSnapshot){
 	    		  Object document = dataSnapshot.getValue();
@@ -57,21 +128,32 @@ public class App
 			}
 	      });
 	      
-	      final FirebaseDatabase database = FirebaseDatabase.getInstance();
-	      DatabaseReference ref1 = database.getReference("https://javajikken.firebaseio.com/");
-	      DatabaseReference usersRef = ref1.child("users");
-	      
+	    
+
 	      Map<String,User> users = new HashMap<String, User>();
 	      users.put("alanisawesome", new User("June 23,1912","Alan Turing"));
-	      users.put("gracehop", new User("December 9,1906","Grace Hopper"));
 	      
-	      usersRef.setValue(users);
-
+	      
+	     //ここでデータをデータベースに書き込む//
+	     ref.setValue(users, new DatabaseReference.CompletionListener(){
+	  			public void onComplete(DatabaseError dataerror, DatabaseReference dataref) {
+					if(dataerror != null){
+						System.out.println("Data could not be saved" + dataerror);
+					}
+					else {
+						System.out.println("Data saved successfully.");
+					}
 	      }
+	      });
+	
+	      }
+
 	
 	
     public static void main( String[] args ) throws FileNotFoundException 
     {
         configFirebase();
+        new frame();
+        
     }
 }
